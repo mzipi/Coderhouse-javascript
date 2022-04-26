ca5f8c2f520052912fddcd7423e6fe2631316486
Michell Zipitría <mzipi@outlook.com>
2022-04-26 18:16:31 -0300

    notificaciones toastify agregadas

--- js/scripts.js
+++ js/scripts.js
@@ -59,6 +59,13 @@ function botonMas () {
             document.getElementById("menos1").removeAttribute("disabled");
             document.getElementById("menos1").setAttribute("enabled", "");
             contador1++;
+            Toastify({
+                text: "Agregaste alfajor al contador",
+                style: {
+                    background: "#804000",
+                },
+                gravity: "bottom"
+            }).showToast();
             break;
         case "boton2":
             calorias += arreglo[1].calorias;
@@ -66,6 +73,13 @@ function botonMas () {
             document.getElementById("menos2").removeAttribute("disabled");
             document.getElementById("menos2").setAttribute("enabled", "");
             contador2++;
+            Toastify({
+                text: "Agregaste ensalada al contador",
+                style: {
+                    background: "#00aa00",
+                },
+                gravity: "bottom"
+            }).showToast();
             break;
         case "boton3":
             calorias += arreglo[2].calorias;
@@ -73,6 +87,13 @@ function botonMas () {
             document.getElementById("menos3").removeAttribute("disabled");
             document.getElementById("menos3").setAttribute("enabled", "");
             contador3++;
+            Toastify({
+                text: "Agregaste pan al contador",
+                style: {
+                    background: "#cccc00",
+                },
+                gravity: "bottom"
+            }).showToast();
             break;
         case "boton4":
             calorias += arreglo[3].calorias;
@@ -80,6 +101,13 @@ function botonMas () {
             document.getElementById("menos4").removeAttribute("disabled");
             document.getElementById("menos4").setAttribute("enabled", "");
             contador4++;
+            Toastify({
+                text: "Agregaste carne al contador",
+                style: {
+                    background: "#aa0000",
+                },
+                gravity: "bottom"
+            }).showToast();
             break;
         default:
             break;
@@ -97,6 +125,13 @@ function botonMenos () {
                 document.getElementById("menos4").removeAttribute("enabled");
             }
             calorias -= arreglo[0].calorias;
+            Toastify({
+                text: "Eliminaste un alfajor del contador",
+                style: {
+                    background: "#804000",
+                },
+                gravity: "bottom"
+            }).showToast();
             break;
         case "menos2":
             contador2--;
@@ -105,6 +140,13 @@ function botonMenos () {
                 document.getElementById("menos4").removeAttribute("enabled");
             }
             calorias -= arreglo[1].calorias;
+            Toastify({
+                text: "Eliminaste una porción de ensalada del contador",
+                style: {
+                    background: "#00aa00",
+                },
+                gravity: "bottom"
+            }).showToast();
             break;
         case "menos3":
             contador3--;
@@ -113,6 +155,13 @@ function botonMenos () {
                 document.getElementById("menos4").removeAttribute("enabled");
             }
             calorias -= arreglo[2].calorias;
+            Toastify({
+                text: "Eliminaste una porción de pan del contador",
+                style: {
+                    background: "#cccc00",
+                },
+                gravity: "bottom"
+            }).showToast();
             break;
         case "menos4":
             contador4--;
@@ -121,6 +170,13 @@ function botonMenos () {
                 document.getElementById("menos4").removeAttribute("enabled");
             }
             calorias -= arreglo[3].calorias;
+            Toastify({
+                text: "Eliminaste un porción de carne del contador",
+                style: {
+                    background: "#aa0000",
+                },
+                gravity: "bottom"
+            }).showToast();
             break;
         default:
             break;
